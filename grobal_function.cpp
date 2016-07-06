9e219edfa952de0b86789eb32dcce838dce8d1d5
takeuchi-moe <takeuchi-moe-gj@ynu.jp>
2016-07-06 14:36:07 +0900

    ipdated

--- grobal_function.cpp
+++
@@ -1,5 +0,0 @@
-#define INT_COMP 1e-2								// intŒ^“¯m‚ÌœZ‚Ì‚Æ‚«‚ÌŒë·•â³—pŒW”
-#define CARRY_DIV_ORD 1e+10								// Œ…ã‚°œZ‚Å‚ÌŒ…ã‚°‚Ì”
-//#define INT_DIV(x, y) ((x * CARRY_DIV_ORD) / (y * CARRY_DIV_ORD) + INT_COMP)		// Œ…ã‚°œZ
-#define INT_DIV(x, y) ((int) (((double) x / (double) y) + INT_COMP))		// Œ…ã‚°œZ
-#define SQ(x) (x * x)									// 2æ
